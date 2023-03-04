3ac6c9033b609c8435203b80b3bf6edd1c6fa94e
karl <jstadcyccnt@gmail.com>
2023-03-04 17:46:56 +0800

    add cors origins

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from  scraper import Scraper
 
 app = FastAPI()
 data = Scraper()
+
+origins = [
+    # "http://localhost.tiangolo.com",
+    # "https://localhost.tiangolo.com",
+    "",
+    "https://floating-spire-08257.herokuapp.com",
+    "http://localhost:3000"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get('/')
